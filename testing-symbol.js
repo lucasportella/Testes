9b963e56e38c33a86e6bbcd69e28a077d470a000
lucas <lucasportellaagu@gmail.com>
2021-11-26 21:19:51 -0300

    real example

--- testing-symbol.js
+++ testing-symbol.js
@@ -22,5 +22,23 @@ console.log(Reflect.ownKeys(myObj))
 
 
 
-
 // https://medium.com/intrinsic-blog/javascript-symbols-but-why-6b02768f4a5c
+
+// testing with mongo(mocha chai test objectId from mongo):
+
+    it('successfully gets all tasks', async () => {
+      db.collection('tasks').insertMany(data);
+      const response = await tasksModel.getTasks();
+      expect(response).to.be.a('array');
+      expect(response).to.have.lengthOf(3);
+      response.forEach((task) => {
+        expect(task).to.be.a('object');
+        console.log(task);
+        console.log(Reflect.ownKeys(task._id));
+        console.log(task['_id']);
+        const symbol = Reflect.ownKeys(task._id)[0]
+        expect(symbol).to.be.a('symbol')
+        expect(task).to.have.all.keys('_id', 'name', 'status', 'date');
+      });
+    });
+  });

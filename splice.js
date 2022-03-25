80b1acf0bf94545203424ca0c5306c82f4560ab8
lucas <lucasportellaagu@gmail.com>
2022-03-24 21:02:43 -0300

    [docs]: adds a correct documentation for splice method splice method can not only delete but change the array values in the operation, so the syntax should be delteOrChangeCount. Examples were provided.

---
+++ splice.js
@@ -0,0 +1,12 @@
+const myArray = [1,2,3]
+// Syntax
+// splice(start)
+// splice(start, deleteOrChangeCount)
+// splice(start, deleteOrChangeCount, item1)
+// splice(start, deleteOrChangeCount, item1, item2, itemN)
+
+myArray.splice(1, 1, 'b', 'c')
+console.log(myArray); // [ 1, 'b', 3 ]
+
+myArray.splice(2,1,'d')
+console.log(myArray) // [ 1, 'b', 'c' ]
